08c22ac7d5d2b9778b407d36ba1cca5a52cd1013
Fancyears <1243093769@qq.com>
2018-04-27 20:12:05 +0800

    1.可返回JSON数据 2.可返回String类型 3.返回网页可能还有问题，因为返回网页不是JSON类型的

--- src/main/java/com/csdn/demo/sys/controller/LoadBomDataController.java
+++ src/main/java/com/csdn/demo/sys/controller/LoadBomDataController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.csdn.demo.common.base.controller.GenericController;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import sql.BaseSQLUtil;
 import sql.IBaseSQLUtil;
 import sql.pojo.HzBomLineRecord;
@@ -21,8 +23,10 @@ public class LoadBomDataController {
 
 	IBaseSQLUtil baseSQLUtil;
 
-	@RequestMapping(value = "/loadByID", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/loadByID", method = RequestMethod.GET)
+	@ResponseBody
 	public String getLineRecords(@RequestParam String bdf) {
+		
 		System.out.println("------开始-----");
 		if (baseSQLUtil == null) {
 			baseSQLUtil = new BaseSQLUtil();
@@ -33,12 +37,37 @@ public class LoadBomDataController {
 		result = baseSQLUtil.executeQuery(bomLineRecord, "sql.mapper.i.HzBomLineRecordMapper.selectByBomDigifaxId");
 		System.out.println("----成功----");
 		JSONArray array = new JSONArray();
-		if(result.size()==0)
-			return null;
+		if (result.size() == 0)
+			return "---";
 		for (HzBomLineRecord hbr : result) {
 			array.add(hbr.getIndex());
 		}
-		return "sys/tree/add";
+		return "bom";
+	}
+
+	@RequestMapping(value = "/loadByID2", method = RequestMethod.GET)
+	//是返回值，否则会返回一个网页
+	@ResponseBody
+	public JSONObject getLineRecord_2(@RequestParam String bdf) {
+		System.out.println("------开始-----");
+		if (baseSQLUtil == null) {
+			baseSQLUtil = new BaseSQLUtil();
+		}
+		List<HzBomLineRecord> result = null;
+		HzBomLineRecord bomLineRecord = new HzBomLineRecord();
+		bomLineRecord.setBomDigifaxId(bdf.trim()/* "d050cb5e-2f54-4782-92dc-6cf750e0b066" */);
+		result = baseSQLUtil.executeQuery(bomLineRecord, "sql.mapper.i.HzBomLineRecordMapper.selectByBomDigifaxId");
+		System.out.println("----成功----");
+		JSONArray array = new JSONArray();
+		// if (result.size() == 0) {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("key", "map");
+		return jsonObject;
+		// }
+		// for (HzBomLineRecord hbr : result) {
+		// array.add(hbr.getIndex());
+		// }
+		// return null;
 	}
 
 	public static void main(String[] args) {
