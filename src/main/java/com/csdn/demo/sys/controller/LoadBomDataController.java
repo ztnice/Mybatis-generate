14d6a3014dd2aefd88b8e58e0a5ada6c773289a9
Fancyears <1243093769@qq.com>
2018-04-27 21:13:41 +0800

    asd

--- src/main/java/com/csdn/demo/sys/controller/LoadBomDataController.java
+++ src/main/java/com/csdn/demo/sys/controller/LoadBomDataController.java
@@ -25,8 +25,8 @@ public class LoadBomDataController {
 
 	@RequestMapping(value = "/loadByID", method = RequestMethod.GET)
 	@ResponseBody
-	public String getLineRecords(@RequestParam String bdf) {
-		
+	public JSONArray getLineRecords(@RequestParam String bdf) {
+
 		System.out.println("------开始-----");
 		if (baseSQLUtil == null) {
 			baseSQLUtil = new BaseSQLUtil();
@@ -37,16 +37,20 @@ public class LoadBomDataController {
 		result = baseSQLUtil.executeQuery(bomLineRecord, "sql.mapper.i.HzBomLineRecordMapper.selectByBomDigifaxId");
 		System.out.println("----成功----");
 		JSONArray array = new JSONArray();
-		if (result.size() == 0)
-			return "---";
-		for (HzBomLineRecord hbr : result) {
-			array.add(hbr.getIndex());
-		}
-		return "bom";
+		if (result.size() > 0)
+			for (HzBomLineRecord hbr : result) {
+				JSONObject object=new JSONObject();
+				object.put("index", hbr.getIndex());
+				object.put("bdf", hbr.getBomDigifaxId());
+				object.put("on", hbr.getOrderNum());
+				object.put("puid", hbr.getPuid());
+				array.add(object);
+			}
+		return array;
 	}
 
 	@RequestMapping(value = "/loadByID2", method = RequestMethod.GET)
-	//是返回值，否则会返回一个网页
+	// 是返回值，否则会返回一个网页
 	@ResponseBody
 	public JSONObject getLineRecord_2(@RequestParam String bdf) {
 		System.out.println("------开始-----");
@@ -68,6 +72,13 @@ public class LoadBomDataController {
 		// array.add(hbr.getIndex());
 		// }
 		// return null;
+	}
+
+	@RequestMapping(value = "/loadByID3", method = RequestMethod.GET)
+	// 是返回值，否则会返回一个网页
+	public String getLineRecord_3() {
+		System.out.println("------开始-----");
+		return "bom";
 	}
 
 	public static void main(String[] args) {
